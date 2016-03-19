7ab78be33237ec923f6402efb0e33a50dc2c62f4
Felix Maurer <Felix.Maurer@gmx.net>
2016-03-19 13:03:21 +0100

    Add layout components

--- app/layouts/app.tsx
+++ app/layouts/app.tsx
@@ -1,11 +1,29 @@
 import  * as React from 'react';
-import { AppBar } from 'material-ui';
+import { LeftNav, AppBar } from 'material-ui';
+var injectTapEventPlugin = require('react-tap-event-plugin');
+injectTapEventPlugin();
+var layout = require('react-layout-components');
 
 export default class App extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        this.state = {sidebarOpen: false};
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({sidebarOpen: open});
+    }
+
+    toggleSidebar() {
+        this.setState({sidebarOpen: !this.state.sidebarOpen});
+    }
+
     render() {
-        return <div>
-            <AppBar title="Coeo" />
+        return <layout.Page>
+            <LeftNav docked={false} open={this.state.sidebarOpen} onRequestChange={open => this.onSetSidebarOpen(open)}>
+            </LeftNav>
+            <AppBar title="Coeo" onLeftIconButtonTouchTap={this.toggleSidebar.bind(this)} />
             <div>{this.props.children}</div>
-            </div>;
+            </layout.Page>
     }
 }
