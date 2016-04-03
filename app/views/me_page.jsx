fb04b2598292f00d45c50a98a7da35a8e54399a9
Felix Maurer <Felix.Maurer@gmx.net>
2016-04-03 21:43:16 +0200

    Improve me page

--- app/views/me_page.jsx
+++ app/views/me_page.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { maybeGet } from '../state/objects';
+import { Card, CardTitle, CardMedia } from 'material-ui';
+import { Container } from 'react-layout-components';
 
 class MePage extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    componentWillMount() {
+        this.props.loadSocial(this.props.currentUser);
+    }
+
     render() {
         let fullName = this.props.socialMe ? this.props.socialMe.data.fullName : '';
         return (
-            <div>{fullName}</div>
+            <Container maxWidth={900} margin="20px auto">
+                <Card>
+                    <CardMedia
+                        overlay={<CardTitle title={fullName} />} >
+                        <img src="https://unsplash.it/900/300" />
+                    </CardMedia>
+                </Card>
+            </Container>
         );
     }
 }
 
 const mapStateToProps = (state) => {
     return {
+        currentUser: state.session.user,
         socialMe: state.objects[state.session.user+'/soc/me']
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {}
+    return {
+        loadSocial: (user) => {
+            dispatch(maybeGet(user+'/soc/me'));
+        }
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MePage);
