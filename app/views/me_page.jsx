d8f1a59d8ab09d90ea2d5461335d7adf156100bb
Felix Maurer <Felix.Maurer@gmx.net>
2016-04-03 23:34:14 +0200

    Load motto in me view

--- app/views/me_page.jsx
+++ app/views/me_page.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { maybeGet } from '../state/objects';
-import { Card, CardTitle, CardMedia } from 'material-ui';
+import { Card, CardHeader, CardMedia } from 'material-ui';
 import { Container } from 'react-layout-components';
+import { Avatar } from 'material-ui';
+import SocialPerson  from 'material-ui/lib/svg-icons/social/person';
 
 class MePage extends React.Component {
     constructor(props) {
@@ -10,17 +12,20 @@ class MePage extends React.Component {
     }
 
     componentWillMount() {
-        this.props.loadSocial(this.props.currentUser);
+        this.props.loadSocial(this.props.user);
     }
 
     render() {
-        let { socialMe: { data: { fullName } ={} } ={} } = this.props;
+        let {
+            me: { data: { fullName } ={} } ={},
+            motto: { data: motto } ={}
+        } = this.props;
         return (
             <Container maxWidth={900} margin="20px auto">
                 <Card>
                     <CardMedia
-                        overlay={<CardTitle title={fullName} />} >
-                        <img src="https://unsplash.it/900/300" />
+                        overlay={<CardHeader avatar={<Avatar icon={<SocialPerson />} />} title={fullName} subtitle={motto} />} >
+                        <img src="https://unsplash.it/900/200" height="200" />
                     </CardMedia>
                 </Card>
             </Container>
@@ -29,9 +34,14 @@ class MePage extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+    let user = state.session.user;
+    if (!user) {
+        return {};
+    }
     return {
-        currentUser: state.session.user,
-        socialMe: state.objects[state.session.user+'/soc/me']
+        user: user,
+        me: state.objects[user+'/soc/me'],
+        motto: state.objects[user+'/soc/me/motto']
     };
 }
 
@@ -39,6 +49,7 @@ const mapDispatchToProps = (dispatch) => {
     return {
         loadSocial: (user) => {
             dispatch(maybeGet(user+'/soc/me'));
+            dispatch(maybeGet(user+'/soc/me/motto'))
         }
     };
 }
