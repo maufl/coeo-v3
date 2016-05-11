26777d9569d4b56019faae55a6a8b49d83411313
Felix Konstantin Maurer <maufl@maufl.de>
2016-05-11 21:58:02 +0200

    Navigate to login page on reload

--- app/bootstrap.jsx
+++ app/bootstrap.jsx
@@ -4,11 +4,13 @@ import routes from './routes';
 import store from './store';
 import { responsiveListen } from './state/responsive';
 import { Provider } from 'react-redux';
+import { push } from 'react-router-redux';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 store.dispatch(responsiveListen());
+store.dispatch(push("/login"));
 
 document.addEventListener("DOMContentLoaded", function(event) {
     ReactDOM.render(
